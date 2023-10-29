e52694b722985d66b588c94fa46831063ea4f616
Eloann <eloann76@gmail.com>
2023-10-29 23:07:34 +0100

    nouveau push

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 fetch("data/movies.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        console.log(data["results"])
+  .then(response => response.json())
+  .then(data => {
+    const movieList = document.querySelector(".movie-list");
 
-        data.results.forEach(function (movie) {
-            var div = document.createElement('div')
+    data.results.forEach(function (movie) {
+      const card = document.createElement('div');
+      card.classList.add('card');
 
+      card.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="card-img-top" alt="affiches films">
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">${movie.release_date}</p>
+          <a href="movie.html?id=${movie.id}" class="btn btn-secondary view-more">Voir Plus</a>
+        </div>
+      `;
 
-            div.innerHTML = `
-            <div class="card" style="width: 18rem;">
-            <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="card-img-top" alt="affiches films">
-            <div class="card-body">
-              <h5 class="card-title">${movie.title}</h5>
-              <p class="card-text">${movie.release_date} </p>
-              <a href="#" class="btn btn-primary">Voir plus</a>
-            </div>
-          </div>
+      movieList.appendChild(card);
+    });
 
-`
-            document.body.appendChild(div)
-        })
-    })
+    // Ajouter un événement de clic aux boutons "Voir Plus"
+    const viewMoreButtons = document.querySelectorAll('.view-more');
+    viewMoreButtons.forEach(button => {
+      button.addEventListener('click', function (e) {
+        // Empêcher le lien par défaut pour rester sur la même page
+        e.preventDefault();
+        
+        // Récupérer l'ID du film depuis l'attribut "href"
+        const movieId = this.getAttribute('href').split('=')[1];
+
+        // Rediriger vers la page "movie.html" avec l'ID du film
+        window.location.href = `movie.html?id=${movieId}`;
+      });
+    });
+  });
 
 
