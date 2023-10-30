df07a1c58216bd618e90039183f27375f4487fa9
Eloann <eloann76@gmail.com>
2023-10-30 11:12:07 +0100

    modif link

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 fetch("data/movies.json")
   .then(response => response.json())
   .then(data => {
-    const movieList = document.querySelector(".movie-list");
+    const movieList = document.querySelector(".movie-list")
     let index = 0
     data.results.forEach(function (movie) {
 
-      const card = document.createElement('div');
-      card.classList.add('card');
-
+      const card = document.createElement('div')
+      card.classList.add('card', 'col-lg-3', 'col-10', 'm-3')
       card.innerHTML = `
         <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" class="card-img-top" alt="affiches films">
         <div class="card-body">
           <h5 class="card-title">${movie.title}</h5>
           <p class="card-text">${movie.release_date}</p>
-          <a href="movie.html?id=${index}" class="btn btn-secondary view-more">Voir Plus</a>
+          <a href="movie.html?index=${index}" class="btn btn-secondary view-more">Voir Plus</a>
         </div>
-      `;
+      `
 
-      movieList.appendChild(card);
+      movieList.appendChild(card)
       index++
-    });
-
-
-  });
+    })
+  })
 
 
