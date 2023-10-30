9d68477958592b503b7aa1623fac56e56fd6ed23
Eloann <eloann76@gmail.com>
2023-10-30 10:11:08 +0100

    maj

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ fetch("data/movies.json")
   .then(response => response.json())
   .then(data => {
     const movieList = document.querySelector(".movie-list");
-
+    let index = 0
     data.results.forEach(function (movie) {
+
       const card = document.createElement('div');
       card.classList.add('card');
 
@@ -12,27 +13,15 @@ fetch("data/movies.json")
         <div class="card-body">
           <h5 class="card-title">${movie.title}</h5>
           <p class="card-text">${movie.release_date}</p>
-          <a href="movie.html?id=${movie.id}" class="btn btn-secondary view-more">Voir Plus</a>
+          <a href="movie.html?id=${index}" class="btn btn-secondary view-more">Voir Plus</a>
         </div>
       `;
 
       movieList.appendChild(card);
+      index++
     });
 
-    // Ajouter un événement de clic aux boutons "Voir Plus"
-    const viewMoreButtons = document.querySelectorAll('.view-more');
-    viewMoreButtons.forEach(button => {
-      button.addEventListener('click', function (e) {
-        // Empêcher le lien par défaut pour rester sur la même page
-        e.preventDefault();
-        
-        // Récupérer l'ID du film depuis l'attribut "href"
-        const movieId = this.getAttribute('href').split('=')[1];
 
-        // Rediriger vers la page "movie.html" avec l'ID du film
-        window.location.href = `movie.html?id=${movieId}`;
-      });
-    });
   });
 
 
